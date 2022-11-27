cdbd2595a5f2d0fd6bf710731d152d403763eac8
kinari321 <abcdream321@yahoo.co.jp>
2022-11-27 09:47:44 +0900

    dokcerが動くように修正

--- software-design-201711/env_script/postgres_initialize.sh
+++ software-design-201711/env_script/postgres_initialize.sh
@@ -11,11 +11,11 @@ docker rm -f postgres_sd psql_sd || true
 # running postgres on docker
 docker network create postgre_default || true
 
-docker build -t postgres_sd:9.6.5 .
-docker run --net postgre_default --net-alias postgres_sd --name postgres_sd -d postgres_sd:9.6.5
+docker build -t postgres_sd:latest .
+docker run --net postgre_default --net-alias postgres_sd --name postgres_sd -d postgres_sd:latest
 
 # psql container
-docker pull postgres:9.6.5
+docker pull postgres:latest
 sleep 10
 
 cat <<EOS
@@ -25,7 +25,7 @@ cat <<EOS
 #================
 EOS
 
-while !(docker run -it --net postgre_default --net-alias psql_sd --name psql_sd postgres:9.6.5 psql -U postgres -h postgres_sd -p 5432)
+while !(docker run -it --net postgre_default --net-alias psql_sd --name psql_sd postgres:latest psql -U postgres -h postgres_sd -p 5432)
 do
   echo "wait PostgreSQL server..."
   docker rm -f psql_sd || true
